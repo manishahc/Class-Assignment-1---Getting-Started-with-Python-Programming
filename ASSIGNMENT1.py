df356c9c668d967c8c03d7f7a97c504b70e8108e
manishahc <70005254+manishahc@users.noreply.github.com>
2020-08-28 22:16:13 +0530

    Add files via upload

---
+++ ASSIGNMENT1.py
@@ -0,0 +1,50 @@
+
+# ASSIGNMENT 1
+
+#2
+print("Name: MANISHA")
+print("Address: Latha Bhavan, Valiyavila, Vattiyoorkavu, Trivandrum")
+print("Phone No: 9495678929")
+
+
+#3
+name = "Manisha"
+print("Your name is",name)
+
+
+#4
+width = int(input("Enter the width: "))
+height = int(input("Enter the height: "))
+area = width*height
+print("The area of rectangle is ",area,"square units")
+
+
+#5
+base = int(input("enter the base: "))
+height = int(input("enter the height: "))
+area = 0.5*base*height
+print("The area of triangle is",area,"square units")
+
+
+#6
+radius = int(input("enter the radius: "))
+area = 3.14*radius**2
+print("The area of circle is",area,"square units")
+
+
+#7
+name = input("enter the name: ")
+age = int(input("enter the age: "))
+print("Name is",name,"and age is",age)
+
+
+#8
+num = int(input("enter a number: "))
+num = num+1 #this line doesn't work because we are not printing this statement so the output will be enter the number and it doesn't print adding 1 to it.
+
+
+#9
+name = input("enter your first name")
+
+
+
